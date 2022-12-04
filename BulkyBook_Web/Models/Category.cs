f797673b0694bed137052d865c18facbae8e91b5
Amir Hamza <hamza_amir@yahoo.com>
2022-12-04 11:30:21 +0600

    4.Section 2 - Add Data Annotations

--- BulkyBook_Web/Models/Category.cs
+++ BulkyBook_Web/Models/Category.cs
@@ -1,10 +1,19 @@
-﻿namespace BulkyBook_Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook_Web.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+
         public int DisplayOrder { get; set; }
+
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
