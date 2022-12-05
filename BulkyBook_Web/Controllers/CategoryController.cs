9e64162fb1e3273d67e2500d28fdb95712f5b1da
Amir Hamza <hamza_amir@yahoo.com>
2022-12-05 10:41:06 +0600

    13.Section 3 - Create Category View

--- BulkyBook_Web/Controllers/CategoryController.cs
+++ BulkyBook_Web/Controllers/CategoryController.cs
@@ -18,5 +18,11 @@ namespace BulkyBook_Web.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
