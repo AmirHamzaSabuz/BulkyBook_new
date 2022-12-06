7dc47f4a225ddecae8602cd2d1c585d1bfc4296e
Amir Hamza <hamza_amir@yahoo.com>
2022-12-07 01:36:51 +0600

    31.Section 5 - Replace DbContext with Category Repository

--- BulkyBook_Web/Controllers/CategoryController.cs
+++ BulkyBook_Web/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿
 using BulkyBook_DataAccess.Data;
+using BulkyBook_DataAccess.Repository.IRepository;
 using BulkyBook_Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,16 +8,16 @@ namespace BulkyBook_Web.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly ICategoryRepository _db;
 
-        public CategoryController(ApplicationDbContext db)
+        public CategoryController(ICategoryRepository db)
         {
             _db = db;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _db.Categories;
+            IEnumerable<Category> objCategoryList = _db.GetAll();
             return View(objCategoryList);
         }
 
@@ -38,8 +39,8 @@ namespace BulkyBook_Web.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
+                _db.Add(obj);
+                _db.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -54,9 +55,9 @@ namespace BulkyBook_Web.Controllers
             {
                 return NotFound();
             }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            
+            var categoryFromDbFirst = _db.GetFirstOrDefault(u => u.Id == id);
+            
 
             if (categoryFromDbFirst == null)
             {
@@ -77,8 +78,8 @@ namespace BulkyBook_Web.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _db.Update(obj);
+                _db.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
@@ -93,9 +94,9 @@ namespace BulkyBook_Web.Controllers
             {
                 return NotFound();
             }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            
+            var categoryFromDbFirst = _db.GetFirstOrDefault(u => u.Id == id);
+            
 
             if (categoryFromDbFirst == null)
             {
@@ -110,14 +111,14 @@ namespace BulkyBook_Web.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult DeletePOST(int? id)
         {
-            var obj = _db.Categories.Find(id);
+            var obj = _db.GetFirstOrDefault(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
 
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _db.Remove(obj);
+            _db.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
--- BulkyBook_Web/Program.cs
+++ BulkyBook_Web/Program.cs
@@ -1,5 +1,7 @@
 
 using BulkyBook_DataAccess.Data;
+using BulkyBook_DataAccess.Repository.IRepository;
+using BulkyBook_DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
