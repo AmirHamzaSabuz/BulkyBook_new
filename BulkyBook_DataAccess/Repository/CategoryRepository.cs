76df228d41eb66fc2a235f6c5f98c27b75143291
Amir Hamza <hamza_amir@yahoo.com>
2022-12-07 00:10:46 +0600

    30.Section 5 - Implement ICategoryRepository CategoryRepository

---
+++ BulkyBook_DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBook_DataAccess.Data;
+using BulkyBook_DataAccess.Repository.IRepository;
+using BulkyBook_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+
+    }
+}
