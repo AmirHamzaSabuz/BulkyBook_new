174c2f446ae4bcc9332fe2b5be8c075302de1add
Amir Hamza <hamza_amir@yahoo.com>
2022-12-10 11:51:47 +0600

    51.Section 6 - Include Properties

--- BulkyBook_Web/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook_Web/Areas/Admin/Controllers/ProductController.cs
@@ -131,7 +131,7 @@ namespace BulkyBook_Web.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productList = _unitOfWork.Product.GetAll();
+            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = productList });
         }
         #endregion
