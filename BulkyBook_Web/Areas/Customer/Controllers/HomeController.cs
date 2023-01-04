da144e6d554188c72d943e4d31f4c4dba0a3be13
Amir Hamza <hamza_amir@yahoo.com>
2023-01-05 00:54:56 +0600

    57.Section 7 - Home Page

--- BulkyBook_Web/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook_Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿
+using BulkyBook_DataAccess.Repository.IRepository;
+using BulkyBook_Models;
 using BulkyBook_Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +11,18 @@ namespace BulkyBook_Web.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = 
+                _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
